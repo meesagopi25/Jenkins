f8a75e345b1b6742619352fa8922ccae2f1527e8
meesagopi25 <meesagopi25@gmail.com>
2025-12-04 13:33:30 +0530

    Update app.py
    v2

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello from Flask on OpenShift Sandbox!", 200
+
+@app.route("/health")
+def health():
+    return jsonify(status="UP"), 200
+
+if __name__ == "__main__":
+    # Listen on all interfaces, port 5000 (matches Dockerfile & Kubernetes)
+    app.run(host="0.0.0.0", port=5000)
 
